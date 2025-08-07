f538261ef95e27d8c7282d5405221df03659b633
subbu <subbunandyala48@gmail.com>
2025-08-07 21:58:37 +0530

    k8's

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -10,4 +10,4 @@ fi
 # Making it as available in env
 export MYSQL_ROOT_PASSWORD=$PASSWORD
 rm -rf /tmp/mysql_root_password.txt
-exec /entrypoint.sh mysqld
+exec  /usr/local/bin/docker-entrypoint.sh mysqld
