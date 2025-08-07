7c73368d095eb488fa732f56ed8582e973854ec8
subbu <subbunandyala48@gmail.com>
2025-08-07 21:33:38 +0530

    docker

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,14 +1,13 @@
-#!/bin/bash 
+#!/bin/bash
 
-if [ -f /tmp/mysql_root_password.txt ]; then 
-   PASSWORD=$(cat /tmp/mysql_root_password.txt) 
-   echo "Accessed root password" 
-else 
-   echo "Password file not found" 
-fi 
-# making it as available env 
-export MYSQL_ROOT_PASSWORD=$PASSWORD 
-
-rm -f /tmp/mysql_root_password.txt 
-
-exec /entrypoint.sh mysqld 
+if [ -f /tmp/mysql_root_password.txt ]; then
+    PASSWORD=$(cat /tmp/mysql_root_password.txt)
+    echo "Accessed Root password"
+else
+    echo "Password file not found"
+    exit 1
+fi
+# Making it as available in env
+export MYSQL_ROOT_PASSWORD=$PASSWORD
+rm -rf /tmp/mysql_root_password.txt
+exec /entrypoint.sh mysqld
